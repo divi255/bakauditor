0d188885561a5a8dfb5a410379e43ade0b7380d8
divisor <div@altertech.com>
2019-10-07 03:40:52 +0200

    zfs size

--- bakauditor/plugins/zfs.py
+++ bakauditor/plugins/zfs.py
@@ -10,22 +10,32 @@ from types import SimpleNamespace
 def get_zfs_snapshots(ssh=None):
     ssh_cmd = '' if not ssh else 'ssh {} '.format(ssh)
     result = []
-    with os.popen('{}zfs list -t snapshot'.format(ssh_cmd)) as p:
-        for s in p.readlines():
-            result.append(s.split()[0])
+    with os.popen('{}zfs list -p -t snapshot'.format(ssh_cmd)) as p:
+        for s in p.readlines()[1:]:
+            try:
+                path, size = s.split()[:2]
+                result.append((path, size))
+            except:
+                pass
     return result
 
 
 def check(**kwargs):
     fs = kwargs['fs']
     t = 0
-    for s in get_zfs_snapshots(kwargs.get('ssh')):
+    size = None
+    for snap in get_zfs_snapshots(kwargs.get('ssh')):
+        s = snap[0]
         if s.startswith(fs + '@'):
             try:
                 t = max(
                     t,
                     datetime.strptime(s.split('@')[1],
                                       kwargs.get('time-fmt')).timestamp())
+                try:
+                    size = int(snap[1])
+                except:
+                    pass
             except:
                 pass
-    return SimpleNamespace(ok=t > 0, time=t, size=None, err=None)
+    return SimpleNamespace(ok=t > 0, time=t, size=size, err=None)
